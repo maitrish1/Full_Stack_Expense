0d7e0bf8c634116a2d3aeaef74b5761989fedd81
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-28 18:57:26 +0530

    ec2 config commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('@sequelize/core');
 const { MySqlDialect } = require('@sequelize/mysql');
+const dotenv=require('dotenv')
+dotenv.config()
 
 const sequelize = new Sequelize({
     dialect: MySqlDialect,
-    database: 'expensesapp',
-    user: 'root',
-    password: 'Babadook@gublu11',  // Notice the change from 'user' to 'username'
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASS,  // Notice the change from 'user' to 'username'
     host: 'localhost',
     port: 3306,
     logging: console.log
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const https = require('https');
@@ -13,10 +14,8 @@ const expenseRoutes = require('./routes/expenseRoutes.js');
 const paymentRoutes = require('./routes/paymentRoutes.js');
 const ForgotPasswordRequests = require('./models/ForgotPassword.js');
 
-// Create an express application
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
@@ -24,14 +23,12 @@ app.use(helmet());
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 app.use(morgan('combined', { stream: accessLogStream }));
 
-// Define the models relationships
 User.hasMany(Expense, { foreignKey: 'userId' });
 Expense.belongsTo(User, { foreignKey: 'userId' });
 
 User.hasMany(ForgotPasswordRequests, { foreignKey: 'userId' });
 ForgotPasswordRequests.belongsTo(User, { foreignKey: 'userId' });
 
-// Routes
 app.use('/user', userRoutes);
 app.use('/expense', expenseRoutes);
 app.use('/premium', paymentRoutes);
@@ -40,16 +37,13 @@ app.get('/', (req, res) => {
   res.json('App is running');
 });
 
-// Read SSL certificate files
 const privateKey = fs.readFileSync('server.key', 'utf8');
 const certificate = fs.readFileSync('server.cert', 'utf8');
 
-// Create HTTPS server
 https.createServer({ key: privateKey, cert: certificate }, app).listen(8800, () => {
   console.log('Server running on https://localhost:8800');
 });
 
-// Sync sequelize with the database
 sequelize.sync()
   .then(() => {
     console.log('Synced with database');
